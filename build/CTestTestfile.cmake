9815ca05975c21a1aaf05c00eda782261ff7e192
Piotr Slawinski <piotr.r.slawinski@gmail.com>
2016-02-12 17:43:38 -0600

    Reorganizing.

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sitl/UAV_Class/uav_simulation/src
-# Build directory: /home/sitl/UAV_Class/uav_simulation/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/sitl/UAV_Class/uav_simulation/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/sitl/storm_lab_research/ros_workspace/devel;/opt/ros/indigo".split(';') if "/home/sitl/storm_lab_research/ros_workspace/devel;/opt/ros/indigo" != "" else []
